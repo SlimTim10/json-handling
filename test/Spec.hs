c251d21e2a04bcc95e8ed93d940d4c901688282e
SlimTim10 <slimtim10@gmail.com>
2020-03-08 19:15:18 -0400

    Rename tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,14 +7,20 @@ import Assert
   , assertEither
   )
 
-main :: IO ()
-main = do
+testSingle = do
   putStrLn "Single pet"
   pet <- readPet "test/1.json"
   assertEither pet (Pet {name = "Nermal", species = Cat, colour = "grey", age = 3})
+
+testMultiple = do
   putStrLn "Multiple pets"
   pets <- readPets "test/2.json"
   assertEither pets
     [ Pet {name = "Nermal", species = Cat, colour = "grey", age = 3}
     , Pet {name = "Garfield", species = Cat, colour = "orange", age = 10}
     ]
+
+main :: IO ()
+main = do
+  testSingle
+  testMultiple
